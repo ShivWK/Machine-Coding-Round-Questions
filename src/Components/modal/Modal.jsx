55cea140121f25803520252822af678a0b4df867
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-03-21 15:56:22 +0530

    Fix the inert not removing isuue

--- src/Components/modal/Modal.jsx
+++ src/Components/modal/Modal.jsx
@@ -1,66 +1,118 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
-import styles from "./modal.module.css";
 import { X } from 'lucide-react';
+import "./modal.css";
 
 const Modal = ({
     open,
     onClose,
     children,
     className,
-    showClasses,
-    hideClasses,
+    animation = "slide",
     title
 }) => {
-    const drawer = useRef(null);
     const lastFocusedElement = useRef(null);
+    const backdropRef = useRef(null);
+    const dialogRef = useRef(null);
+    const elementsRef = useRef([]);
+
+    useEffect(() => {
+        elementsRef.current = [
+            document.getElementById('root'),
+            document.getElementById("toast-root"),
+        ].filter(Boolean)
+    }, [])
+
+    const lockOutsideAccess = useCallback(() => {
+        document.body.style.overflow = 'hidden';
+
+        elementsRef.current.forEach((ele) => {
+            ele.setAttribute("inert", "")
+            ele.setAttribute("aria-hidden", "true")
+        })
+    }, [])
+
+    const allowOutsideAccess = useCallback(() => {
+        document.body.style.overflow = 'auto';
+
+        elementsRef.current.forEach((ele) => {
+            ele.removeAttribute("inert")
+            ele.removeAttribute("aria-hidden")
+        })
+    }, [])
+
+    const handleClose = useCallback(() => {
+        const appliedAnimation = animation === "pop" ? "popOut__animation" : "slideOut__animation";
+
+        backdropRef.current.classList.add("fadeOut__animation");
+        dialogRef.current.classList.add(appliedAnimation);
+
+        const handleAnimationEnd = () => {
+            allowOutsideAccess()
+            lastFocusedElement.current?.focus();
+            onClose()
+        }
+
+        dialogRef.current.addEventListener("animationend", handleAnimationEnd, {
+            once: true
+        })
+    }, [animation, onClose, allowOutsideAccess])
 
     useEffect(() => {
         if (open) {
             lastFocusedElement.current = document.activeElement;
-            document.getElementsByTagName('main')[0]?.setAttribute('inert', '');
-            document.getElementsByTagName('header')[0]?.setAttribute('inert', '');
-            document.body.style.overflow = 'hidden';
-            drawer.current?.focus();
-        } else {
-            document.getElementsByTagName('main')[0]?.removeAttribute('inert');
-            document.getElementsByTagName('header')[0]?.removeAttribute('inert');
-            document.body.style.overflow = 'auto';
-            lastFocusedElement.current?.focus();
+            dialogRef.current?.focus();
+            lockOutsideAccess()
         }
-    }, [open, drawer, lastFocusedElement]);
+
+        // Below will not run on time, before which component will unmount. When open becomes false, the modal unmounts before the effect can run, so the cleanup logic inside the effect never executes.
+
+        // else {
+        //     allowOutsideAccess()
+        //     lastFocusedElement.current?.focus();
+        // }
+    }, [open, lockOutsideAccess]);
 
     useEffect(() => {
         if (!open) return;
 
         const handleKeyPress = (e) => {
-            if (e.key === 'Escape') onClose();
+            if (e.key === 'Escape') handleClose();
         };
 
-        document.addEventListener('keydown', handleKeyPress);
+        document.addEventListener('keyup', handleKeyPress);
         return () => {
-            document.removeEventListener('keydown', handleKeyPress);
+            document.removeEventListener('keyup', handleKeyPress);
         };
-    }, [open, onClose]);
+    }, [open, handleClose]);
+
+    useEffect(() => {
+        return () => {
+            allowOutsideAccess();
+        };
+    }, [allowOutsideAccess]);
 
     return createPortal(
         <div
-            onClick={onClose}
+            role='presentation'
             aria-hidden="true"
-            className={`${styles["dialog__overlay"]}`}
+            ref={backdropRef}
+            onClick={handleClose}
+            className="dialog__backdrop"
         >
             <div
-                onClick={(e) => e.stopPropagation()}
-                ref={drawer}
-                aria-modal="true"
-                aria-hidden={!open}
                 role="dialog"
+                aria-modal="true"
+                onClick={(e) => e.stopPropagation()}
+                ref={dialogRef}
                 tabIndex={-1}
-                className={`${styles["dialog__box"]} ${className} ${open ? showClasses : hideClasses}`}
+                className={`dialog__box ${className} ${animation === "slide"
+                    ? "slideIn__animation"
+                    : "popIn__animation"}`}
             >
-                <button className={`${styles["dialog__closeBtn"]}`} onClick={onClose}><X /></button>
-                <h2 className={`${styles["dialog__heading"]}`}>{title}</h2>
-                <div className={`${styles["dialog__content"]}`}>{children}</div>
+                {onClose && <button className="dialog__closeBtn" onClick={handleClose}><X /></button>}
+                <h2 className="dialog__heading" >{title}</h2>
+                <div className="dialog__content" >{children}</div>
             </div>
         </div>,
         document.getElementById("modal-root"),
