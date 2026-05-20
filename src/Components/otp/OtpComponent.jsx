005825646913ea45987f985d20d8ad3c4b0cbfd7
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-20 14:05:44 +0530

    Refactor the Otp component code

--- src/Components/otp/OtpComponent.jsx
+++ src/Components/otp/OtpComponent.jsx
@@ -4,27 +4,52 @@ import "./otp.css";
 const Otp = ({ count = 4 }) => {
     const numberOfDigits = Array.from({ length: Number(count) }, (_, i) => i);
     const [otp, setOtp] = useState(new Array(count).fill(""));
+    // Filling with "" prevents undefined values.
+    // Undefined can cause an uncontrolled → controlled input warning.
     const inputRef = useRef([]);
+
+    const moveFocusRight = (index) => {
+        if (inputRef.current[index + 1]) {
+            inputRef.current[index + 1].focus();
+        }
+    }
+
+    const moveFocusLeft = (index) => {
+        if (inputRef.current[index - 1]) {
+            inputRef.current[index - 1]?.focus();
+        }
+    }
 
     const handleInputChange = (e, index) => {
         const value = e.target.value.trim();
-        if (isNaN(value)) return;
+        if (value && !/^\d+$/.test(value)) return;
 
         const newArray = [...otp];
         newArray[index] = value.slice(-1);
         setOtp(newArray);
 
-        if (index !== otp.length - 1 && value) {
-            inputRef.current[index + 1].focus();
+        if (value) {
+            moveFocusRight(index);
         }
     }
 
-    const handelOnKeyDown = (e, index) => {
-        const value = e.target.value;
-        if (!value && e.key === "Backspace") {
-            if (index !== 0) {
-                inputRef.current[index - 1]?.focus();
+    const handleOnKeyDown = (e, index) => {
+        if (e.key === "Backspace") {
+            e.preventDefault();
+            const newOtp = [...otp];
+
+            if (otp[index]) {
+                newOtp[index] = "";
+            } else if (index > 0) {
+                newOtp[index - 1] = "";
+                moveFocusLeft(index);
             }
+
+            setOtp(newOtp);
+        } else if (e.key === "ArrowRight") {
+            moveFocusRight(index);
+        } else if (e.key === "ArrowLeft") {
+            moveFocusLeft(index);
         }
     }
 
@@ -37,15 +62,16 @@ const Otp = ({ count = 4 }) => {
             <h1>Validate OTP</h1>
             <div className="otp-container">
                 {
-                    numberOfDigits.map((input, index) => {
+                    numberOfDigits.map((_, index) => {
                         return <input
                             key={index}
                             type="text"
                             value={otp[index]}
+                            inputMode="numeric"
                             onChange={(e) => handleInputChange(e, index)}
-                            onKeyDown={(e) => handelOnKeyDown(e, index)}
+                            onKeyDown={(e) => handleOnKeyDown(e, index)}
                             className="otp-input"
-                            ref={(input) => inputRef.current[index] = input}
+                            ref={(ele) => inputRef.current[index] = ele}
                         />
                     })
                 }
