38143739b3ed8a6a41fe7391f716921127fd4508
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-24 02:35:36 +0530

    handle start pause resume reset operations

--- src/Components/timer/Timer.jsx
+++ src/Components/timer/Timer.jsx
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./timer.css";
 import { TimeFactors, Config } from "./utils";
 
 const Timer = () => {
     const [config, setConfig] = useState(structuredClone(Config));
+    const [totalSeconds, setTotalSeconds] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
+
+    const interval = useRef(null)
 
     const OrderOfTimer = [TimeFactors.Hour, TimeFactors.Minute, TimeFactors.Second];
+
+    const resetTimer = () => {
+        clearInterval(interval.current);
+
+        setIsRunning(false);
+        setIsPaused(false);
+        setTotalSeconds(0);
+
+        setConfig(structuredClone(Config));
+    }
+
+    useEffect(() => {
+        if (!isRunning) return;
+
+        interval.current = setInterval(() => {
+            setTotalSeconds(prv => {
+                if (prv <= 1) {
+                    resetTimer()
+                    return 0;
+                }
+
+                return prv - 1;
+            })
+        }, 1000)
+
+        return () => {
+            clearInterval(interval.current)
+        }
+    }, [isRunning])
 
     const handleChange = (e, { key }) => {
         const value = e.target.value;
@@ -21,16 +55,33 @@ const Timer = () => {
     }
 
     const handleStart = () => {
+        const hour = Number(config[TimeFactors.Hour].value || 0);
+        const minutes = Number(config[TimeFactors.Minute].value || 0);
+        const seconds = Number(config[TimeFactors.Second].value || 0);
 
+        const total = hour * 3600 + minutes * 60 + seconds;
+
+        if (total <= 0) return;
+        setTotalSeconds(total);
+        setIsRunning(true);
     }
 
     const handlePause = () => {
+        clearInterval(interval.current);
 
+        setIsRunning(false);
+        setIsPaused(true);
     }
 
-    const handleReset = () => {
-
+    const handleResume = () => {
+        if (totalSeconds <= 0) return;
+        setIsRunning(true);
+        setIsPaused(false);
     }
+
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
 
     return (
         <div className="timer-parent">
@@ -44,8 +95,11 @@ const Timer = () => {
 
                             return <div key={orderKey} className="timer-input-wrapper">
                                 <input
+                                    disabled={isRunning || isPaused}
                                     onChange={(e) => handleChange(e, { key: orderKey, index })}
-                                    value={data.value}
+                                    value={(isRunning || isPaused) ?
+                                        [hours, minutes, seconds][index].toString().padStart(2, "0")
+                                        : data.value}
                                     type="text"
                                     placeholder={data.placeholder}
                                     className="timer-output"
@@ -56,15 +110,23 @@ const Timer = () => {
                 </div>
 
                 <div className="timer-controls">
-                    <button onClick={handleStart} className="timer-button">
+                    <button
+                        onClick={handleStart}
+                        disabled={isPaused || isRunning}
+                        className="timer-button"
+                    >
+                        Start
+                    </button>
+
+                    {isRunning && <button onClick={handlePause} className="timer-button">
                         Pause
-                    </button>
+                    </button>}
 
-                    <button onClick={handlePause} className="timer-button">
+                    {isPaused && <button onClick={handleResume} className="timer-button">
                         Resume
-                    </button>
+                    </button>}
 
-                    <button onClick={handleReset} className="timer-button">
+                    <button onClick={resetTimer} className="timer-button">
                         Reset
                     </button>
                 </div>
